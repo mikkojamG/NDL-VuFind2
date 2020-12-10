14684efb19ff6ec65a29c3ddfe8706289ea4d4f7
mikkojamG <mikko.jamia@gofore.com>
2020-12-10 14:01:59 +0200

    Only add required components for now

--- themes/finna2/components.config.php
+++ themes/finna2/components.config.php
@@ -1,10 +1,8 @@
 <?php
-$config['js'][] = 'components/molecules/lists/contact/contact.js';
-$config['js'][] = 'components/molecules/lists/service/service.js';
 $config['js'][] = 'components/molecules/info-block/service-point/service-point-info.js';
 $config['js'][] = 'components/molecules/media/service-point-building/service-point-building.js';
+$config['js'][] = 'components/molecules/lists/contact/contact.js';
+$config['js'][] = 'components/molecules/lists/service/service.js';
 $config['js'][] = 'components/molecules/scheduling/service-point-schedule/service-point-schedule.js';
 $config['js'][] = 'components/organisms/feed/organisation/organisation.js';
-$config['js'][] = 'components/organisms/keywords/keywords/keywords.js';
 $config['js'][] = 'components/organisms/map/service-points/service-points.js';
-$config['js'][] = 'components/organisms/scheduling/widget/widget.js';
