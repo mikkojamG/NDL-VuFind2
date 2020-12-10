63bb291d9e2d8c4000fdc964d77f854ddd17cf5f
mikkojamG <mikko.jamia@gofore.com>
2020-12-10 14:04:17 +0200

    Remove unused components for now

--- themes/finna2/components.config.php
+++ themes/finna2/components.config.php
@@ -1,7 +1,7 @@
 <?php
+$config['js'][] = 'components/molecules/lists/contact/contact.js';
 $config['js'][] = 'components/molecules/info-block/service-point/service-point-info.js';
 $config['js'][] = 'components/molecules/media/service-point-building/service-point-building.js';
-$config['js'][] = 'components/molecules/lists/contact/contact.js';
 $config['js'][] = 'components/molecules/lists/service/service.js';
 $config['js'][] = 'components/molecules/scheduling/service-point-schedule/service-point-schedule.js';
 $config['js'][] = 'components/organisms/feed/organisation/organisation.js';
